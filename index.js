aa1f543eb588e9eacda4978258b83e336019c465
hhvolodymyrhh <167300466+hhvolodymyrhh@users.noreply.github.com>
2024-08-27 17:54:14 +0000

    Deploying to gh-pages from @ hhvolodymyrhh/goit-js-hw-11@7d6259d81e58c838a51887769ec3272fdbd5c64c 🚀

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const n=document.querySelector("body"),u=`<div class="container">
-    <form class="formFetchEl">
-		<input type="text" class="search-input" name="Search" />
-		<button class="btnEl">Search</button>
-    </form>
-    
-	  <ul class="galleryEl"></ul>
-</div>`;n.insertAdjacentHTML("afterbegin",u);const d=document.querySelector("form");document.querySelector(".btnEl");d.addEventListener("submit",r=>{r.preventDefault(),m().then(s=>p(s)).catch(s=>console.log(s))});async function m(r){document.querySelector(".galleryEl");const s=new URLSearchParams({key:"45653057",q:"name",image_type:"photo",orientation:"horizontal",safesearch:"true"}),a=await fetch(`https://pixabay.com/api/?${s}`);if(!a.ok)throw new Error(a.status);return await a.json()}function p(r){const s=r.map(({webformatURL:a,largeImageURL:l,tags:e,likes:t,views:o,comments:c,downloads:i})=>`<li class="gallery-list-item>
-            <a class="gallery-link" href="${l}">
-                    <img class="img" src="${a}" 
-                        alt="${e}" 
-                        title="${e}" />
+import{S as d,i as m}from"./assets/vendor-5ObWk2rO.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function p(a){const r=new URLSearchParams({key:"25786434-348adb767e319176b4ad356ea",q:`${a}`,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${r}`).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}function f(a,r){console.log(a);const s=a.map(({webformatURL:o,largeImageURL:e,tags:t,likes:i,views:n,comments:c,downloads:u})=>`<li class="gallery-list-item>
+            <a class="gallery-link" href="${e}">
+                    <img class="img" src="${o}" 
+                        alt="${t}" 
+                        title="${t}" />
                     <ul class="data-list">
                         <li class="data-item">
                             <p class="data-item-name">Likes</p>
-                            <p class="data-likes">${t}</p>
+                            <p class="data-likes">${i}</p>
                         </li>
                         <li class="data-item">
                             <p class="data-item-name">Views</p>
-                            <p class="data-views">${o}</p>
+                            <p class="data-views">${n}</p>
                         </li>
                         <li class="data-item">
                             <p class="data-item-name">Comments</p>
@@ -25,9 +18,16 @@
                         </li>
                         <li class="data-item">
                             <p class="data-item-name">Downloads</p>
-                            <p class="data-downloads">${i}</p>
+                            <p class="data-downloads">${u}</p>
                         </li>
                     </ul>
                 </a> 
-          </li>`).join("");console.log(s),userList.insertAdjacentHTML("beforeend",s)}
+          </li>`).join("");r.insertAdjacentHTML("beforeend",s)}const h=document.querySelector("body"),y=`<div class="container">
+    <form class="formFetchEl">
+		<input type="text" class="search-input" name="search" />
+		<button class="btnEl">Search</button>
+    </form>
+    
+	  <ul class="galleryEl"></ul>
+</div>`;h.insertAdjacentHTML("afterbegin",y);console.log(d);const g=document.querySelector("form"),l=document.querySelector(".galleryEl");g.addEventListener("submit",a=>{a.preventDefault();const r=a.currentTarget.elements.search.value.toLowerCase().trim();r&&p(r).then(s=>{if(s.hits.length===0){m.show({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#000",messageSize:"18px",messageLineHeight:"20px",backgroundColor:"rgb(255,153,102)",position:"topRight"});const e=document.querySelector(".iziToast");e.style.borderRadius="10px",e.style.overflow="hidden"}else{l.innerHTML="",f(s.hits,l);var o=$(".galleryEl a").simpleLightbox();o.refresh()}}).catch(s=>console.log(s))});
 //# sourceMappingURL=index.js.map
